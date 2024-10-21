bcb68d31d6fd0126d7b26379b1235285c0556520
SaidYandarbiev <Said.Yandarbiev@student.uantwerpen.be>
2024-10-21 13:05:03 +0200

    Added tf_idf weighting and changed a small part in tokenization

--- Document_retrieval_system.py
+++ Document_retrieval_system.py
@@ -1,7 +1,8 @@
+from collections import defaultdict
 import os
 import re
 from time import sleep
-import math
+from math import log
 import pandas
 
 directory = './full_docs_small'
@@ -13,6 +14,7 @@ def intersection(lst1, lst2):
 def main():
     inverted_index_docs = {}
     file_count = 0
+    document_map = {}
     for file in os.listdir(directory):
         file_count += 1
         if file.endswith(extension):
@@ -27,27 +29,27 @@ def main():
                 for word in wordlist:
                     # Uppercase split
                     # Example: "ProblemsPesticides" -> "Problems", "Pesticides"
-                    word = re.sub(r'(?<=[A-Z])(?=[A-Z][a-z])', ' ', word)
-
+                    word = re.sub(r'(?<=[a-z])(?=[A-Z])', ' ', word)
                     # Number split
                     # Example "hyper10" -> "hyper", "10"
                     word = re.sub(r'(?<=[a-zA-Z])(?=\d)|(?<=\d)(?=[a-zA-Z])', ' ', word)
-                    
+                    word = word.lower()
                     # Add the modified words to the list
                     modified_wordlist.extend(word.split())
-            
+
+                filename = os.path.basename(document.name)
+                document_map[filename] = modified_wordlist
+
                 word_count = 0
                 for words in modified_wordlist:
                     word_count += 1               
-                    if len(words) > 1:
-                        if words not in inverted_index_docs.keys():
-                            inverted_index_docs[words] = {}
-                        if file not in inverted_index_docs[words].keys():    
-                            inverted_index_docs[words][file] = [word_count]
-                        else:
-                            inverted_index_docs[words][file].append(word_count)
+                    if words not in inverted_index_docs.keys():
+                        inverted_index_docs[words] = {}
+                    if file not in inverted_index_docs[words].keys():    
+                        inverted_index_docs[words][file] = [word_count]
+                    else:
+                        inverted_index_docs[words][file].append(word_count)
                 #print(inverted_index_docs) 
-
 
     file = pandas.read_excel('dev_small_queries.xlsx')
     query_numbers = file['Query number'].tolist()
@@ -55,13 +57,19 @@ def main():
     new_queries = []
     for query in queries:
         text = query.split()
-        new_queries.append(text)
+        modified_wordlist = []
+        for word in text:
+            word = re.sub(r'(?<=[A-Z])(?=[A-Z][a-z])', ' ', word)
+            word = re.sub(r'(?<=[a-zA-Z])(?=\d)|(?<=\d)(?=[a-zA-Z])', ' ', word)              
+            modified_wordlist.extend(word.split())
+        new_queries.append(modified_wordlist)
     query_mapping = {}
     index = 0
     for query in new_queries:
         current_query = []
         cur_loop = 0       
         for word in query:
+            
             if word in inverted_index_docs.keys():
                 if len(current_query) == 0 and cur_loop == 0:
                     current_query = list(inverted_index_docs[word].keys())
@@ -83,29 +91,45 @@ def main():
         # Write each key-value pair to the file in a formatted way
             file.write(f"{key}: {', '.join(map(str, value))}\n")
 
-    term_frequencies = {}
-    for query in new_queries:
-        for word in query:
-            if word in inverted_index_docs.keys():
-                docmap = inverted_index_docs[word]
-                for docs in docmap.keys():
-                    if docs not in term_frequencies.keys():
-                        term_frequencies[docs] = {}
-                    term_frequencies[docs][word] = len(inverted_index_docs[word][docs])             
-    
-    document_term_frequency = {}
-    for word in inverted_index_docs.keys():
-        document_term_frequency[word] = len(inverted_index_docs[word])
+    tf_idf_weighting = defaultdict(dict)
+    for doc_id, tokens in document_map.items():
+        tf = {}
+        for word in tokens:
+            if word not in tf:
+                tf[word] = 0
+            tf[word] += 1
+        for word in tf.keys():
+            tf[word] /= len(tokens)
+            df = len(inverted_index_docs[word])
+            idf = log((file_count/ df + 1))
+            tf_idf = tf[word] * idf
+            tf_idf_weighting[doc_id][word] = tf_idf
 
-    inverse_document_frequency = {}
-    for word in document_term_frequency.keys():    
-        inverse_document_frequency[word] = math.log10(file_count/document_term_frequency[word])
+    # term_frequencies = {}
+    # for query in new_queries:
+    #     for word in query:
+    #         if word in inverted_index_docs.keys():
+    #             docmap = inverted_index_docs[word]
+    #             for docs in docmap.keys():
+    #                 if docs not in term_frequencies.keys():
+    #                     term_frequencies[docs] = {}
+    #                 term_frequencies[docs][word] = len(inverted_index_docs[word][docs])             
     
-    tf_idf_weighting = {}
-    for word in document_term_frequency.keys():
-        tf_idf_weighting[word] = (1 + math.log10(document_term_frequency[word])) * inverse_document_frequency[word]
+    # document_term_frequency = {}
+    # for word in inverted_index_docs.keys():
+    #     document_term_frequency[word] = len(inverted_index_docs[word])
 
-    print(tf_idf_weighting)
+    # inverse_document_frequency = {}
+    # for word in document_term_frequency.keys():    
+    #     inverse_document_frequency[word] = math.log10(file_count/document_term_frequency[word])
+    
+    # tf_idf_weighting = {}
+    # for doc in term_frequencies.keys():
+    #     tf_idf_weighting[doc] = {}
+    #     for word in document_term_frequency.keys():
+    #         tf = 1 + math.log10(term_frequencies[doc][word]) if word in term_frequencies[doc] else 0
+    #         tf_idf_weighting[doc][word] = tf
+
  
     return 0
 
