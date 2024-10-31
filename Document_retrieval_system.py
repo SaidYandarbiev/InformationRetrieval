97024a43be1b76965ac5d759d2546cb9d420c0d5
SaidYandarbiev <Said.Yandarbiev@student.uantwerpen.be>
2024-10-31 10:56:05 +0100

    Fixed the low MAP@K value issue and split the preprocessing and the construction of inverted index

--- Document_retrieval_system.py
+++ Document_retrieval_system.py
@@ -1,40 +1,45 @@
 from collections import defaultdict
+from concurrent.futures import ProcessPoolExecutor
+import json
 import os
+import sqlite3
 from time import sleep
 from math import log, sqrt
 from math import log, sqrt
 import pandas
 from preprocess import Preprocessor
 import csv
+import time
 
 directory = './full_docs_small'
 extension = '.txt'
 radius = 10
-k = 10
+k = 3
+max_terms_in_memory = 1000
 
-def cosine_similarity(vec_a, vec_b, file, query):
+#This function determines the cosine similarity for a given query and document
+def cosine_similarity(vec_a, vec_b):
     dot_product = 0
     magnitude_a = 0
     magnitude_b = 0
 
-    new_vec_b = {}
-    for word in vec_a.keys():
-        if word in vec_b:
-            dot_product += vec_a[word] * vec_b[word]
-            new_vec_b[word] = vec_b[word]
-    for value in vec_a.values():
-        magnitude_a += value**2
-    for value in new_vec_b.values():
-        magnitude_b += value**2
-    if (file == "output_590.txt" or file == "output_80.txt") and query == 1089273:
-        print(file)
-        print(magnitude_a)
-        print(magnitude_b)
+    # Combine terms in both vectors
+    all_terms = set(vec_a.keys()).union(set(vec_b.keys()))
+
+    # Calculate dot product and magnitudes
+    for word in all_terms:
+        a_val = vec_a.get(word, 0)
+        b_val = vec_b.get(word, 0)
+        dot_product += a_val * b_val
+        magnitude_a += a_val ** 2
+        magnitude_b += b_val ** 2
+
+    # Avoid division by zero
     if magnitude_a == 0 or magnitude_b == 0:
         return 0
+    return dot_product / (sqrt(magnitude_a) * sqrt(magnitude_b))
 
-    return dot_product/ (sqrt(magnitude_a) * sqrt(magnitude_b))                
-
+#This function normalizes the tf_idf values so that they can be used to calculate the cosine similarity
 def normalize(vector):
     norm = 0
     for value in vector:
@@ -45,10 +50,6 @@ def normalize(vector):
     for i in range(0, len(vector)):
         vector[i] = vector[i]/norm    
     return vector
-
-def intersection(lst1, lst2):
-    return list(set(lst1).intersection(set(lst2)))
-
 
 def main():
     inverted_index_docs = {}
@@ -63,14 +64,13 @@ def main():
                 modified_wordlist = preprocessor.preprocess(text)    
                 filename = os.path.basename(document.name)
                 document_map[filename] = modified_wordlist
-                word_count = 0
-                for words in modified_wordlist:
-                    word_count += 1             
-                    if words not in inverted_index_docs.keys():
-                        inverted_index_docs[words] = {}    
-                    if file not in inverted_index_docs[words].keys():    
-                        inverted_index_docs[words][file] = 0
-                    inverted_index_docs[words][file] += 1
+
+    for file, wordlist in document_map.items():
+        for word in wordlist:
+            if word not in inverted_index_docs.keys():
+                inverted_index_docs[word] = []   
+            if file not in inverted_index_docs[word]:    
+                inverted_index_docs[word].append(file)
 
     file = pandas.read_excel('dev_small_queries.xlsx')
     query_numbers = file['Query number'].tolist()
@@ -88,17 +88,17 @@ def main():
                 tf[word] = 0
             tf[word] += 1 
         for word, freq in tf.items():
-            tf = 1 + log(freq)
-            df = len(inverted_index_docs[word].keys())
+            tf_value = 1 + log(freq)
+            df = len(inverted_index_docs[word])
             idf = log(file_count/df)
-            document_tf_idf[doc_id][word] = tf * idf 
+            document_tf_idf[doc_id][word] = tf_value * idf 
+
     for doc_id in document_tf_idf.keys():
         wordlist = []
         normalizelist = []
         for word in document_tf_idf[doc_id].keys():
             wordlist.append(word)
             normalizelist.append(document_tf_idf[doc_id][word])
-   
         normalizelist = normalize(normalizelist)
         for i in range(0, len(wordlist)):
             document_tf_idf[doc_id][wordlist[i]] = normalizelist[i]
@@ -112,15 +112,15 @@ def main():
                 tf[word] = 0
             tf[word] += 1 
         for word, freq in tf.items():
-            tf = 1 + log(freq)
+            tf_value = 1 + log(freq)
             if word in inverted_index_docs:
-                df = len(inverted_index_docs[word].keys())
+                df = len(inverted_index_docs[word])
                 idf = log(file_count/df)
             else:
                 idf = 0 
-            query_tf_idf[query_numbers[query_count]][word] = tf * idf
+            query_tf_idf[query_numbers[query_count]][word] = tf_value * idf
         query_count += 1        
- 
+
     for query_id in query_tf_idf.keys():
         wordlist = []
         normalizelist = []
@@ -136,7 +136,7 @@ def main():
         query_list = query_tf_idf[query]
         for doc_id in document_tf_idf.keys():
             doc_list = document_tf_idf[doc_id]
-            similarity = cosine_similarity(query_list, doc_list, doc_id, query)
+            similarity = cosine_similarity(query_list, doc_list)
             if similarity > 0:
                 if query not in results:
                     results[query] = []
@@ -148,12 +148,13 @@ def main():
         for similarity in results[query]:
             similarity_list.append(similarity)
         similarity_list.sort(key=lambda x: x[1], reverse=True)
-        # if query == 1077002: #Je kan hier de query nummer vervangen om specifieke queries te checken of dit overeeenkomt met de beste file voor deze query
+        # if query == 1086886: #Je kan hier de query nummer vervangen om specifieke queries te checken of dit overeeenkomt met de beste file voor deze query
         #     print(query)
         #     print(similarity_list)
         #     sleep(100) 
         new_results[query] = similarity_list 
     results = new_results
+
     top_10_results = []
     for query in results.keys():
         i = 0    
@@ -167,7 +168,6 @@ def main():
         fieldnames = ["Query_number", "Doc_number"]
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
-
         for query_result in top_10_results:
             writer.writerow(query_result)
 
@@ -199,6 +199,7 @@ def main():
         total_average_recall += ARK
     MAPK = total_average_precision/len(query_numbers)
     MARK = total_average_recall/len(query_numbers)
+
     print(MAPK)
     print(MARK)
 
